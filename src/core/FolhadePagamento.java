cc17e094907ffe1d44c31b555c482b8c9c848881
WatchEL <gabrielcosta08@gmail.com>
2015-09-20 20:06:22 -0300

    instancias no main

--- src/core/FolhadePagamento.java
+++ src/core/FolhadePagamento.java
@@ -3,7 +3,9 @@ package core;
 public class FolhadePagamento {
 	
 	public static void main(String[] args) {
-		
+		PorComissao porComissao = new PorComissao();
+		PorHora porHora = new PorHora("Gabriel");
+		PorHoraComissao porHoraComissao = new PorHoraComissao();
 	}
 
 }
